4dafa47352a2922d4fcd540a9c260316daa43da1
Crystal Free <cdmackeyfree@gmail.com>
2018-05-20 17:16:47 -0400

    detail pages working woooo

--- faust_store/app/controllers/products_controller.rb
+++ faust_store/app/controllers/products_controller.rb
@@ -2,27 +2,37 @@ class ProductsController < ApplicationController
 require 'csv'
 
   def list
-    @products = []
-    CSV.foreach("/Users/Spoonflower/Desktop/rails-small-business-cdmackeyfree/faust_store/faust_inventory.csv", headers: true) do |row|
-    
-    product = Product.new
-    product.item = row.to_h["item"]
-    product.pid = row.to_h["pid"]
-    product.description = row.to_h["description"]
-    product.price = row.to_h["price"]
-    product.condition = row.to_h["condition"]
-    product.dimension_w = row.to_h["dimension_w"]
-    product.dimension_h = row.to_h["dimension_h"]
-    product.dimension_l = row.to_h["dimension_l"]
-    product.img_file = row.to_h["img_file"]
-    product.quantity = row.to_h["quantity"]
-    product.category = row.to_h["category"]
-
-      @products << row.to_h
-    end
+    @products = fetch_products
+  
   end
 
   def detail
+    @products = fetch_products
+    @product = @products.find{|c| c.pid == params[:pid]}
    
   end
+
+  def fetch_products
+    @products = []
+    CSV.foreach("/Users/Spoonflower/Desktop/rails-small-business-cdmackeyfree/faust_store/faust_inventory.csv", headers: true) do |row|
+    
+      product = Product.new
+      product.item = row.to_h["item"]
+      product.pid = row.to_h["pid"]
+      product.description = row.to_h["description"]
+      product.price = row.to_h["price"]
+      product.condition = row.to_h["condition"]
+      product.dimension_w = row.to_h["dimension_w"]
+      product.dimension_h = row.to_h["dimension_h"]
+      product.dimension_l = row.to_h["dimension_l"]
+      product.img_file = row.to_h["img_file"]
+      product.quantity = row.to_h["quantity"]
+      product.category = row.to_h["category"]
+
+      @products << product
+    end
+    @products
+  end
+
+
 end
--- faust_store/config/routes.rb
+++ faust_store/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'products#list'
 
-  get 'products/:id' => 'products#detail'
+  get 'products/:pid' => 'products#detail'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
